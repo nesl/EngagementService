50a906d36a2f9c4dd700ce6d8ae69f0d38e1494c
Bo-Jhang <TimeStringAlpha@gmail.com>
2018-01-26 10:18:59 -0800

    Checkpoint of whatever changes

--- EngagementService/app/src/main/java/ucla/nesl/engagementservice/MainActivity.java
+++ EngagementService/app/src/main/java/ucla/nesl/engagementservice/MainActivity.java
@@ -1,7 +1,8 @@
 package ucla.nesl.engagementservice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView textView = (TextView) findViewById(R.id.first_label);
         //TODO: a demo app to use our EMA scheduler library
     }
 }
