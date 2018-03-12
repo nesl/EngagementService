bf79a76cebc50ec566e74eb997e1000042796364
Bo-Jhang <TimeStringAlpha@gmail.com>
2018-03-11 19:29:06 -0700

    More analysis

--- HumanModeling/main.py
+++ HumanModeling/main.py
@@ -13,6 +13,7 @@ def main():
     #environment = SurveyUser('survey/ver1_pilot/data/02.txt')
     environment = MTurkSurveyUser([
             'survey/ver2_mturk/results/01_1st_Batch_3137574_batch_results.csv',
+            'survey/ver2_mturk/results/02_Batch_3148398_batch_results.csv',
     ])
     
     controller = Controller(agent, environment)
@@ -22,12 +23,15 @@ def main():
     numNotifications = len(notificationEvents)
     numAcceptedNotifications = len([r for r in notificationEvents if r['reward'] > 0])
     answerRate = numAcceptedNotifications / numNotifications
+    numDismissedNotifications = len([r for r in notificationEvents if r['reward'] < 0])
+    dismissRate = numDismissedNotifications / numNotifications
 
     expectedNumDeliveredNotifications = sum([r['probOfAnswering'] for r in results])
 
     print("%d decision points" % len(results))
-    print("%d notifications are sent, %d are answered (%.2f%%)"
-            % (numNotifications, numAcceptedNotifications, answerRate * 100.))
+    print("%d notifications are sent:" % numNotifications)
+    print("  - %d are answered (%.2f%%)"  % (numAcceptedNotifications, answerRate * 100.))
+    print("  - %d are dismissed (%.2f%%)"  % (numDismissedNotifications, dismissRate * 100.))
     print("Expectation of total delivered notifications is %.2f" % expectedNumDeliveredNotifications)
 
 if __name__ == "__main__":
