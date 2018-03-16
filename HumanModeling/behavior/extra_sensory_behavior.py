d29528bd569e46e7c76605869e202edab00eb8ae
timestring <timestringalpha@gmail.com>
2018-03-15 19:37:24 -0700

    Add regular behavior routings

---
+++ HumanModeling/behavior/extra_sensory_behavior.py
@@ -0,0 +1,104 @@
+import numpy as np
+
+from .base_behavior_model import BaseBehaviorModel
+from constant import *
+
+
+class ExtraSensoryBehavior(BaseBehaviorModel):
+    
+    def __init__(self, sourceFile):
+        """
+        The file format is the following elements separated by tabs:
+            <motion>
+            <location>
+            <second_of_the_week>
+            <day_of_the_week>
+            <hour_of_the_day>
+            <minute_of_the_hour>
+
+        The following is an example:
+        
+            stationary  home    60  0   0   1
+        """
+
+        self.numActivityNan = 0
+        self.numLocationNan = 0
+        with open(sourceFile) as f:
+            self.routing = [self._parseLine(l) for l in f.readlines()]
+
+        if len(self.routing) == 0:
+            raise Exception("ERROR: No routing can be extracted.")
+
+        self.curIdx = 0
+
+    def getLocationActivity(self, hour, minute, day):
+        # handle the rewind case
+        targetTime = (day, minute, hour)
+        if self._getRecordTime(self.curIdx) > targetTime:
+            self.curIdx = 0
+            
+        while self.curIdx < len(self.routing):
+            if self._getRecordTime(self.curIdx) >= targetTime:
+                #print(self.curIdx, self._getRecordLocationActivity(self.curIdx))
+                return self._getRecordLocationActivity(self.curIdx)
+            self.curIdx += 1
+
+        # the end of the routing so have to reset
+        self.curIdx = 0
+        return self._getRecordLocationActivity(self.curIdx)
+
+    def _parseLine(self, line):
+        """
+        Please see `__init__()` for the format detail of a line.
+
+        Returns:
+          (hour, minute, day, stateActivity, stateLocation)
+        """
+        
+        terms = line.strip().split("\t")
+
+        if terms[0] == 'nan':
+            self.numActivityNan += 1
+            terms[0] = 'stationary'
+
+        if terms[1] == 'nan':
+            self.numLocationNan += 1
+            terms[1] = 'others'
+
+        activityToState = {
+            'stationary': STATE_ACTIVITY_STATIONARY,
+            'walking': STATE_ACTIVITY_WALKING,
+            'running': STATE_ACTIVITY_RUNNING,
+            'driving': STATE_ACTIVITY_DRIVING,
+            'commuting': STATE_ACTIVITY_DRIVING,
+        }
+
+        locationToState = {
+            'home': STATE_LOCATION_HOME,
+            'work': STATE_LOCATION_WORK,
+            'others': STATE_LOCATION_OTHER,
+        }
+
+        return (
+                int(terms[4]),
+                int(terms[5]),
+                int(terms[3]),
+                locationToState[terms[1]],
+                activityToState[terms[0]],
+        )
+
+    def _getRecordTime(self, idx):
+        """
+        Returns:
+          (day, hour, minute)
+        """
+        r = self.routing[idx]
+        return (r[2], r[0], r[1])
+
+    def _getRecordLocationActivity(self, idx):
+        """
+        Returns:
+          (stateLocation, stateActivity)
+        """
+        r = self.routing[idx]
+        return (r[3], r[4])
--- HumanModeling/main.py
+++ HumanModeling/main.py
@@ -1,13 +1,14 @@
 from agent import *
 from environment import *
+from behavior import *
 from controller import Controller
 
 def main():
     #agent = AlwaysSendNotificationAgent()
     #agent = QLearningAgent()
-    #agent = QLearningAgent2()
+    agent = QLearningAgent2()
     #agent = ContextualBanditSVMAgent()
-    agent = ContextualBanditSVMProbAgent()
+    #agent = ContextualBanditSVMProbAgent()
 
     #agent = SVMAgent()
     #agent.loadModel('agent/pretrained_models/classifiers/mturk_3000_m3_r1.txt')
@@ -25,10 +26,16 @@ def main():
             'survey/ver2_mturk/results/03_Batch_3149214_batch_results.csv',
     ], filterFunc=(lambda r: ord(r['rawWorkerID'][-1]) % 3 == 2))
     
-    simulationWeek = 10
+    #behavior = RandomBehavior()
+    #behavior = ExtraSensoryBehavior('behavior/data/2.txt')
+    #behavior = ExtraSensoryBehavior('behavior/data/4.txt')
+    #behavior = ExtraSensoryBehavior('behavior/data/5.txt')
+    behavior = ExtraSensoryBehavior('behavior/data/6.txt')
 
-    controller = Controller(agent, environment, simulationWeek=simulationWeek,
-            negativeReward=-5.)
+    simulationWeek = 30
+
+    controller = Controller(agent, environment, behavior,
+            simulationWeek=simulationWeek, negativeReward=-5.)
     results = controller.execute()
 
     numNotifications, numAcceptedNotis, numDismissedNotis = _getResponseRates(results)
