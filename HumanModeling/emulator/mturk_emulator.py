f686aca9e114d0ddc78d6d5167fc6d11fb95c58e
Bo-Jhang <TimeStringAlpha@gmail.com>
2018-03-21 22:52:31 -0700

    Add more analysis in the emulator

--- HumanModeling/emulator/mturk_emulator.py
+++ HumanModeling/emulator/mturk_emulator.py
@@ -2,6 +2,10 @@ import os
 import dill
 import shutil
 import csv
+import math
+import re
+import string
+from string import Template
 
 from utils import utils
 from utils.chronometer import Chronometer
@@ -158,7 +162,7 @@ class MTurkEmulator:
         }
 
         # entire results
-        self.allSimulationResults = []
+        self.allEmulationResults = []
 
         # states to survey labels
         self.activityState2Labels = {
@@ -297,7 +301,7 @@ class MTurkEmulator:
         self.agent.feedBatchRewards(history)
 
         # merge the result back
-        self.allSimulationResults.extend(self.roundNotificationResults)
+        self.allEmulationResults.extend(self.roundNotificationResults)
 
         # update action status
         self.nextAction = MTurkEmulator.ACTION_GENERATING_BATCH_NOTIFICATION
@@ -319,5 +323,113 @@ class MTurkEmulator:
     def getRoundStartEndDays(self):
         return (self.roundStartDay, self.roundEndDay)
 
+    def getAllEmulationResults(self):
+        return self.allEmulationResults
+
+    def getEmulationResultsGroupByWeek(self):
+        numWeeks = math.ceil(self.roundStartDay / 7)
+        recordPartitions = [[] for _ in range(numWeeks)]
+        for r in self.allEmulationResults:
+            week = int(r['context']['numDaysPassed'] / 7)
+            recordPartitions[week].append(r)
+        return recordPartitions
+
+    def getEmulationResultsGroupByDay(self):
+        recordPartitions = [[] for _ in range(self.roundStartDay)]
+        for r in self.allEmulationResults:
+            dayCnt = r['context']['numDaysPassed']
+            recordPartitions[dayCnt].append(r)
+
+    def getEmulationResultsPrintingFormat(self, formatStr):
+        return self._formatEmulationResults(self.allEmulationResults, formatStr)
+
+    def getEmulatorResultsByWeekPrintingFormat(self, formatStr):
+        return [self._formatEmulationResults(partition, formatStr)
+                for partition in self.getEmulationResultsGroupByWeek()]
+    
+    def getEmulatorResultsByDayPrintingFormat(self, formatStr):
+        return [self._formatEmulationResults(partition, formatStr)
+                for partition in self.getEmulationResultsGroupByDay()]
+
+    def printEmulationResults(self, formatStr):
+        print(self.getEmulationResultsPrintingFormat())
+
+    def printEmulationResultsByWeek(self, formatStr):
+        for iWeek, msg in enumerate(self.getEmulatorResultsByWeekPrintingFormat(formatStr)):
+            print("Week %d: %s" % (iWeek, msg))
+
+    def printEmulationResultsByDay(self, formatStr):
+        for iDay, msg in enumerate(self.getEmulatorResultsByDayPrintingFormat(formatStr)):
+            print("Week %d: %s" % (iDay, msg))
+
+    def _formatEmulationResults(self, results, formatStr):
+        """
+        This function summarizes the (subset of) simulation results based on the `formatStr`. For
+        example, if `formatStr` is "The total reward is $totalReward", `$totalReward` will be
+        replaced by the total rewards of `results`.
+
+        The supported keywords are:
+          - $totalReward
+          - $numNotifications
+          - $numAcceptingNotifications
+          - $numIgnoringNotifications
+          - $numDismissingNotifications
+          - $ratioAcceptingNotifications
+          - $ratioIgnoringNotifications
+          - $ratioDismissingNotifications
+          - $ratioAcceptsExcludeIgnores
+        """
+
+        processors = {
+                '$totalReward': lambda records: sum([r['reward'] for r in records]),
+                '$numNotifications': lambda records: self._numNotifications(records),
+                '$numAcceptingNotifications': lambda records: self._numAcceptingNotifications(records),
+                '$numIgnoringNotifications': lambda records: self._numIgnoringNotifications(records),
+                '$numDismissingNotifications': lambda records: self._numDismissingNotifications(records),
+                '$ratioAcceptingNotifications': lambda records: self._getRatio(
+                        self._numAcceptingNotifications(records),
+                        self._numNotifications(records),
+                ),
+                '$ratioIgnoringNotifications': lambda records: self._getRatio(
+                        self._numIgnoringNotifications(records),
+                        self._numNotifications(records),
+                ),
+                '$ratioDismissingNotifications': lambda records: self._getRatio(
+                        self._numDismissingNotifications(records),
+                        self._numNotifications(records),
+                ),
+                '$ratioAcceptsExcludeIgnores': lambda records: self._getRatioAcceptsExcludeIgnores(records),
+        }
+
+        allSymbols = [x for x in re.split('[^a-zA-Z$]', formatStr) if x.startswith("$")]
+        substituteDict = {key[1:]: processors[key](results) for key in allSymbols}
+        return Template(formatStr).substitute(substituteDict)
+
     def _updateFolderPath(self, folderPath):
         self.folderPath = os.path.abspath(folderPath)
+
+    def _filterByWeek(self, results, week):
+        startDay = week * 7
+        endDay = startDay + 7
+        return [r for r in results
+                if startDay <= r['context']['numDaysPassed'] and r['context']['numDaysPassed'] < endDay]
+
+    def _numNotifications(self, results):
+        return len([r for r in results if r['decision']])
+
+    def _numAcceptingNotifications(self, results):
+        return len([r for r in results if r['decision'] and r['reward'] > 0])
+
+    def _numIgnoringNotifications(self, results):
+        return len([r for r in results if r['decision'] and r['reward'] == 0])
+
+    def _numDismissingNotifications(self, results):
+        return len([r for r in results if r['decision'] and r['reward'] < 0])
+
+    def _getRatioAcceptsExcludeIgnores(self, results):
+        numAccepts = self._numAcceptingNotifications(results)
+        numDismisses = self._numDismissingNotifications(results)
+        return self._getRatio(numAccepts, numAccepts + numDismisses)
+
+    def _getRatio(self, numerator, denominator, ndigits=3):
+        return round(numerator / denominator, ndigits) if denominator > 0. else 0.
