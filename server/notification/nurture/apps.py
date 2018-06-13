433d63882c1ae44f955e5bf15faf6b876f28bc6b
timestring <timestringalpha@gmail.com>
2018-06-12 18:19:17 -0700

    Establish Nurture server :

---
+++ server/notification/nurture/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NurtureConfig(AppConfig):
+    name = 'nurture'
---
+++ server/notification/nurture/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def debug_dump_post(request):
+
+    if request.method == 'POST':
+        print(request.POST)
+
+    return HttpResponse("Recv", status=200)
