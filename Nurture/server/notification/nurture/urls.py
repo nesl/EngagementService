9ebffd5ab9f701e481b0e13670f4097a8a37de2e
timestring <timestringalpha@gmail.com>
2018-06-13 18:07:03 -0700

    Add upload-log-file-view

--- Nurture/server/notification/nurture/urls.py
+++ Nurture/server/notification/nurture/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
         url('^get-user-code/$', views.get_user_code, name='get-user-code'),
-        #url('^upload-log-file/$', views.upload_log_file, name='upload-log-file'),
+        url('^upload-log-file/$', views.upload_log_file, name='upload-log-file'),
         #url('^get-action/$', views.get_action, name='get-action'),
         url('^debug-dump-post/$', views.debug_dump_post, name='debug-dump-post'),
 ]
--- Nurture/server/notification/nurture/views.py
+++ Nurture/server/notification/nurture/views.py
@@ -1,8 +1,11 @@
+import pytz
 import random
+import base64
 
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
 
 from nurture.models import *
 
@@ -30,3 +33,45 @@ def get_user_code(request):
     
     AppUser.objects.create(code=code)
     return HttpResponse(code, status=200)
+
+
+@csrf_exempt
+def upload_log_file(request):
+
+    # extract user
+    if 'code' not in request.POST:
+        return HttpResponse("Bad", status=404)
+
+    try:
+        user = AppUser.objects.get(code=request.POST['code'])
+    except AppUser.DoesNotExist:
+        return HttpResponse("Bad", status=404)
+
+    # extract type
+    if 'type' not in request.POST:
+        return HttpResponse("Bad", status=404)
+    type = request.POST['type']
+
+    # extract content
+    if 'content' not in request.POST:
+        return HttpResponse("Bad", status=404)
+    content_base64 = request.POST['content']
+    try:
+        content_bytes = base64.b64decode(content_base64)
+    except:
+        return HttpResponse("Wrong encoding", status=404)
+    
+    # uploaded time
+    now = timezone.now().astimezone(pytz.timezone('US/Pacific'))
+
+    # write file
+    record = FileLog(user=user, type=type, uploaded_time=now)
+    path = record.get_path()
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    with open(path, 'wb') as fo:
+        fo.write(content_bytes)
+
+    record.save()
+
+    return HttpResponse("Ok", status=200)
