6869b6a5f8b6d0628d77296a6ae8a179755a0cbe
timestring <timestringalpha@gmail.com>
2018-06-13 18:37:22 -0700

    [nurture-server] organize views

--- Nurture/server/notification/nurture/urls.py
+++ Nurture/server/notification/nurture/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
 
-from . import views
+from nurture.views import *
 
 urlpatterns = [
 
         ## mobile request
-        url('^mobile/get-user-code/$', views.get_user_code, name='mobile-get-user-code'),
-        url('^mobile/upload-log-file/$', views.upload_log_file, name='mobile-upload-log-file'),
-        url('^mobile/get-action/$', views.get_action, name='mobile-get-action'),
+        url('^mobile/get-user-code/$', mobile_views.get_user_code, name='mobile-get-user-code'),
+        url('^mobile/upload-log-file/$', mobile_views.upload_log_file, name='mobile-upload-log-file'),
+        url('^mobile/get-action/$', mobile_views.get_action, name='mobile-get-action'),
         
         ## debugging purpose
-        url('^debug/dump-post/$', views.debug_dump_post, name='debug-dump-post'),
+        url('^debug/dump-post/$', mobile_views.debug_dump_post, name='debug-dump-post'),
 ]
