cc3ab1f4182819b142c849458181168f959947d0
sypei <38240596+sypei@users.noreply.github.com>
2018-09-26 02:02:09 +0800

    dummy iOS app
    attempt to get screen status and ringtone status. Not working yet but hv got a skeleton.

---
+++ dummy/dummy/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  dummy
+//
+//  Created by Siyou Pei on 2018/9/25.
+//  Copyright © 2018 Siyou Pei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ dummy/dummy/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  dummy
+//
+//  Created by Siyou Pei on 2018/9/25.
+//  Copyright © 2018 Siyou Pei. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "ViewController.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
