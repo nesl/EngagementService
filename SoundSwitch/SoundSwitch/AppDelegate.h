185049977002cd39cf3317743a981a6f61c0963d
sypei <38240596+sypei@users.noreply.github.com>
2018-10-03 20:53:24 +0800

    modify the NSLog macro

--- SoundSwitch/SoundSwitch/AppDelegate.h
+++ SoundSwitch/SoundSwitch/AppDelegate.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 Moshe Gottlieb. All rights reserved.
 //
 #ifdef DEBUG
-#define NSLog(format, ...) printf("%s [第%d行] %s\n", __FUNCTION__, __LINE__, [[NSString stringWithFormat:format, ## __VA_ARGS__] UTF8String]);
+#define NSLog(format, ...) printf("%s [the %dth line] %s\n", __FUNCTION__, __LINE__, [[NSString stringWithFormat:format, ## __VA_ARGS__] UTF8String]);
 #else
 #define NSLog(format, ...)
 #endif
